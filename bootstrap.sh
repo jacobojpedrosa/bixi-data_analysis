0661ad6079d2b97a3cf9ddf5deccbe7862e16d5d
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-MacBook-Pro.local>
2021-08-08 13:24:19 +0200

    Readme y Notebook en html

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo python3 -m pip install spark
+sudo python3 -m pip install pandas
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install s3fs
+sudo python3 -m pip install nltk
+sudo python3 -m pip install ipython-autotime
+sudo python3 -m pip install shapely
+sudo python3 -m pip install geopandas
